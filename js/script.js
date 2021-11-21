545765f0541abcf9f3d41641733645cf65552678
vladp7ay3r <khalipov.vlad@gmail.com>
2021-11-21 03:04:54 +0300

    smooth scroll

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const mobileNav = document.querySelector('.mobile-nav');
 const overflow = document.querySelector('body');
 const tech = document.querySelector('.tech');
 const header = document.querySelector('.header');
-
+const link = document.querySelector('.nav__link');
 
 mobileNavButton.addEventListener('click',function(){
 	mobileNavIcon.classList.toggle('active');
@@ -29,3 +29,27 @@ window.addEventListener("scroll",function(){
 
 
 
+const anchors = document.querySelectorAll('a[href*="#"]')
+for (let anchor of anchors) {
+	anchor.addEventListener('click', function (e) {
+  		e.preventDefault()
+   	const blockID = anchor.getAttribute('href').substr(1)
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+   })
+})
+}
+
+document.querySelectorAll(".nav__link").forEach(nav__link => 
+	nav__link.addEventListener("click", () => mobileNav.classList.remove('active') )
+)
+document.querySelectorAll(".nav__link").forEach(nav__link => 
+	nav__link.addEventListener("click", () => overflow.classList.remove('body--scroll'))
+)
+document.querySelectorAll(".nav__link").forEach(nav__link => 
+	nav__link.addEventListener("click", () => mobileNavIcon.classList.remove('active'))
+)
+document.querySelectorAll(".nav__link").forEach(nav__link => 
+	nav__link.addEventListener("click", () => header.classList.remove('hidden'))
+)
